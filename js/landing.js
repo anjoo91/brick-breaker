696044ff75fbf2e1ddab5604bc32f3e428c5da45
root <root@Xubuntu.myguest.virtualbox.org>
2023-06-16 14:17:25 -0400

    added landing page html, css, and js

---
+++ js/landing.js
@@ -0,0 +1,11 @@
+// Function to start the game
+function startGame() {
+    // Redirect to the game page
+    window.location.href = "index.html";
+}
+
+// Function to view high scores
+function viewScores() {
+    // Redirect to the scores page
+    window.location.href = "scores.html";
+}
